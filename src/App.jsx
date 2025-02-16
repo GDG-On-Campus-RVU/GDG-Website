548c0984731a231f993c474920427ef03de20329
Sushruthvi <sushruthvi3@gmail.com>
2025-02-16 22:30:23 +0530

    blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import Blog from './Components/Blog/Blog.jsx'
 // import TemplatePage from './Components/TemplatePage/TemplatePage.jsx';
 // import Blog from './Components/Blog/Blog.jsx';
 import TeamPage from "./Components/TeamPage/TeamPage.jsx"
+import TemplatePage2 from './Components/TemplatePage/TemplatePage.jsx'
+import Template from './Dump/template.jsx'
 
 function App() {
   return (
     <>
     <div className="sub-root">
-      <TeamPage/>
+      <Blog/>
       {/* <Blog/> */}
     </div>
     </>
