66ef245c3e6552f146d8cb90bc47dbea5998f36c
Vinay-R-S <vinayrsaunshi@gmail.com>
2025-02-16 16:44:42 +0530

    Team page and side nav bar updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import TemplatePage from './Components/TemplatePage/TemplatePage.jsx';
+// import TemplatePage from './Components/TemplatePage/TemplatePage.jsx';
+// import Blog from './Components/Blog/Blog.jsx';
 import TeamPage from "./Components/TeamPage/TeamPage.jsx"
-import Blog from './Components/Blog/Blog.jsx';
+
 function App() {
   return (
     <>
     <div className="sub-root">
       <TeamPage/>
+      {/* <Blog/> */}
     </div>
     </>
   )
