a44dd26caf1d65c14fc4ba9c4cd825009d8ee234
Sri Krishna R Hebbar <srikrishnarhebbar1802@gmail.com>
2025-06-29 13:02:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures that assets are referenced correctly
+  base: '/', // This ensures that assets are referenced correctly
 });
