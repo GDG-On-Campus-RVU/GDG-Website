7c275f21c397df4bf5ad7f31d81dcbdb6e42307e
Sri Krishna R Hebbar <srikrishnarhebbar1802@gmail.com>
2025-06-29 12:01:51 +0530

    Corrected Deployment Issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/GDG-Website/", // 👈 very important for GitHub Pages
   plugins: [react()],
 })
